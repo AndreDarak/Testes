4304adb34a8501e5ecb94536213f628a991815c3
Andre D Avila Silva <andre.avila@unimedlitoral.coop.br>
2024-07-12 08:26:50 -0300

    New Branch

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from time import sleep
+import os
+
 print('testado')
 print('______________________________________________________________________________________________________________')
 print('Aqui foi Testado')
